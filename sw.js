64194e8533e9c94b1f1fe19df50bb160833e24f9
nyihan <37435263+nyihan@users.noreply.github.com>
2025-12-15 07:26:18 +0630

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'myanmar-pos-pro-v86';
+const urlsToCache = [
+  './',
+  './index.html',
+  './dashboard.html',
+  'https://unpkg.com/html5-qrcode',
+  'https://html2canvas.hertzen.com/dist/html2canvas.min.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
